79d5915a59b1a665997c3e18011d1284067feb54
kimidang26 <dangkimberly26@gmail.com>
2023-06-28 21:30:43 -0700

    added airbnb logo

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,15 +1,28 @@
-'use client';
-
-import Container from "../container";
+"use client";
+import Logo from "./Logo"
+import Container from "../Container";
 
 const Navbar = () => {
-    return ( 
-        <div className="fixed w-full bg-white z-10 shadow-sm">
-            <div className="py-4 border-b-[1px]">
-                <Container> Anything </Container>
-            </div>
-        </div>
-     );
-}
- 
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+            <div
+                className="
+                flex
+                flex-row
+                items-center
+                justify-between
+                gap-3
+                md:gap-0
+                "
+            >
+                <Logo />
+            </div> 
+        </Container>
+      </div>
+    </div>
+  );
+};
+
 export default Navbar;
